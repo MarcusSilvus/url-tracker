b67ed23eb98d8cffb9eb481763030c055dcf7f0e
Mark Silva <mark.allen.silva@gmail.com>
2022-08-30 12:46:37 -0700

    js logic to append new values to array and disply them in an ul

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 // 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
 
-let myURLs = [];
+let myURLs = [1, 2, 3, 4];
+let listItems = "";
 
-let savedURLsEl = document.getElementById("savedURLs-el")
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function() {
   myURLs.push(inputEl.value);
   console.log(myURLs)
 
-  for (let i = 0; i < myURLs.length; i++) {
-    savedURLsEl.textContent = myURLs;
-  }
 })
+
+for (let i = 0; i < myURLs.length; i++) {
+  listItems += "<li> " + myURLs[i] + "</li>";
+}
+
+ulEl.innerHTML = listItems;
+
+/* another way to do:
+  ulEl.innerHTML += "<li> " + myURLs[i] + "</li>";
+is:
+  const li = document.createElement("li");
+  li.textContent = myURLs[i];
+  ulEl.append(li);  
+  */
