d033595af3f4822b724210671d7f38b6799114d3
Mark Silva <mark.allen.silva@gmail.com>
2022-08-29 23:17:26 -0700

    created save button to store inputs

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 // 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
 
-let inputBtn = document.getElementById("input-btn");
+let myURLs = [];
+
+let savedURLsEl = document.getElementById("savedURLs-el")
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
 
 inputBtn.addEventListener("click", function() {
-  console.log("BUTTON CLICKED");
+  myURLs.push(inputEl.value);
+  console.log(myURLs)
+
+  for (let i = 0; i < myURLs.length; i++) {
+    savedURLsEl.textContent = myURLs;
+  }
 })
 
+
+
+
+ 
