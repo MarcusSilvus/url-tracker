44933099b6f1ac891e0a6f22f03d186fe442166b
Mark Silva <mark.allen.silva@gmail.com>
2022-08-29 22:46:26 -0700

    added basic styles and js event listner

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // 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
+
+let inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click", function() {
+  console.log("BUTTON CLICKED");
+})
+
